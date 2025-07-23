ba97ed567b1da6b5732936b3022a711981cec1d8
Masume <cetinmasume@gmail.com>
2025-07-23 23:32:00 +0300

    add : widget extension

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by masume çetin on 23.07.2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
