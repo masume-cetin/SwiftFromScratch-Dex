6adcfb4b98c14791932fb108905ea1633b33b680
Masume <cetinmasume@gmail.com>
2025-07-27 23:14:26 +0300

    convert : core data to swift data

--- Dex/FetchService.swift
+++ Dex/FetchService.swift
@@ -14,7 +14,7 @@ struct FetchService {
     private let baseUrl = URL(string: "https://pokeapi.co/api/v2/pokemon")!
     
     
-    func fetchPokemon (_ id : Int) async throws -> FetchedPokemon {
+    func fetchPokemon (_ id : Int) async throws -> Pokemon {
         let fetchUrl = baseUrl.appending(path: String(id))
         let (data,response) = try await URLSession.shared.data(from: fetchUrl)
         guard let response = response as? HTTPURLResponse , response.statusCode == 200 else {
@@ -22,7 +22,7 @@ struct FetchService {
         }
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-        let pokemon = try decoder.decode(FetchedPokemon.self, from: data)
+        let pokemon = try decoder.decode(Pokemon.self, from: data)
         print("Fetched Pokemon : \(pokemon.id) : \(pokemon.name.capitalized)'")
         return pokemon
     }
